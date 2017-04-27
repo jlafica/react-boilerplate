f3e99be3b1979da29d5c8871cd9ae6d9e4268aa1
Jeremiah Lafica <jeremiah.lafica@acefitness.org>
2017-04-27 15:30:15 -0700

    Initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Main = (props) => {
+  return (
+    <div>
+      <p>Main.jsx Rendered</p>
+      {props.children}
+    </div>
+  );
+};
+
+
+module.exports = Main;
